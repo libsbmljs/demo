b39115dff15d2cd5d036eae68a7ee2fc07555c7f
0u812 <0u812@users.noreply.github.com>
2019-01-25 17:45:25 -0800

    use reduced corpus

--- src/database.worker.js
+++ src/database.worker.js
@@ -3,7 +3,8 @@ import { DISPATCH_QUERY } from 'constants.js'
 import { queryResults } from 'actions.js'
 import { List, Map } from 'immutable'
 
-import corpus from '../assets/libsbmljs_demo_corpus.json'
+// import corpus from '../assets/libsbmljs_demo_corpus.json'
+import corpus from '../assets/libsbmljs_demo_corpus_reduced.json'
 import rawidx from '../assets/libsbmljs_demo_index.json'
 const idx = lunr.Index.load(rawidx)
 const documents = Map(List(corpus).map(c => [c.id, c]))
